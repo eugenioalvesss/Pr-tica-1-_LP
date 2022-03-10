9bc95295b79dff48b06feff3ec90e95bfcc08917
eugenioalvesss <84926452+eugenioalvesss@users.noreply.github.com>
2022-03-09 22:30:32 -0300

    Add files via upload

---
+++ Pratica1_LP/src/Atividade/Exercicio2.java
@@ -0,0 +1,47 @@
+package Atividade;
+import java.util.Scanner;
+
+public class Exercicio2 {
+
+	public static void main(String[] args) {
+		
+		Scanner ler = new Scanner (System.in);
+		
+		String placaCarro;
+		short anoFab;
+		String placaFormatada;
+
+		
+		
+		System.out.println("Insira a placa do carro: ");
+		placaCarro = ler.next();
+		
+		placaFormatada = placaCarro.toUpperCase();
+		
+		
+		System.out.println("Insira o ano de fabricação do carro: ");
+		anoFab = ler.nextShort();
+		
+		if (anoFab <= 2010) {
+			System.out.println("Carro velho!");
+
+		} else if (anoFab <= 2021) {
+			System.out.println("Carro semi-novo!");
+			
+		} else if (anoFab == 2022) {
+			System.out.println("Carro novo!");
+		} else {
+			System.out.println("Dados inválidos :(");
+		}
+
+		String placaAlterada = placaFormatada.replace('A', '*')
+		.replace('E', '*')
+		.replace('I', '*')
+		.replace('O', '*')
+		.replace('U', '*');
+		
+		System.out.println(placaAlterada);
+
+	}
+
+}
---
+++ Pratica1_LP/src/Atividade/Exercicio3.java
@@ -0,0 +1,25 @@
+package Atividade;
+
+import java.util.Scanner;
+
+public class Exercicio3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner ler = new Scanner (System.in);
+		
+		String frase;
+		char c;
+		System.out.println("Escreva uma frase: ");
+		frase = ler.next();
+		
+		for (int i = 0; i < frase.length(); i++) {
+			c = frase.charAt(i);
+			
+		}
+		
+
+	}
+
+}
